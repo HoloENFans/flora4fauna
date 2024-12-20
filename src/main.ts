c9cd655f70f64c6f004babc2786b260312431d12
Walt280 <32001362+Walt280@users.noreply.github.com>
2024-12-20 02:45:01 -0500

    Add a (very basic) sample/skeleton app

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,57 @@
+import { Application, Assets, Sprite } from "pixi.js";
+import { Viewport } from "pixi-viewport";
+
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const WORLD_HEIGHT = 5000;
+const WORLD_WIDTH = 5000;
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+async function setup() : Promise<[Application, Viewport]> {
+  const app = new Application();
+
+  await app.init({ background: '#000000', resizeTo: window, backgroundAlpha: 0 });
+  document.getElementById("world-tree")?.appendChild(app.canvas);
+
+  const viewport = new Viewport({
+    screenWidth: window.innerWidth,
+    screenHeight: window.innerHeight,
+    worldWidth: WORLD_WIDTH,
+    worldHeight: WORLD_HEIGHT,
+    events: app.renderer.events, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+  });
+
+  app.stage.addChild(viewport);
+  viewport.drag().pinch().wheel().decelerate();
+
+  return [app, viewport];
+}
+
+async function setupTextures() {
+  const assets = [
+    { alias: 'fauna', src: 'Ceres-Fauna_pr-img_01.png' },
+    { alias: 'smol-fauna', src: 'smol_fauna.png'}
+  ];
+  await Assets.load(assets);
+}
+
+void (async () =>
+{
+  const [app, viewport] = await setup();
+  await setupTextures();
+
+  const kirin = Sprite.from("fauna");
+  kirin.anchor.set(0.5, 0.5);
+  kirin.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2);
+  viewport.addChild(kirin);
+  viewport.moveCenter(WORLD_WIDTH / 2, WORLD_HEIGHT / 2);
+
+  const smolKirin = Sprite.from("smol-fauna");
+  smolKirin.anchor.set(0.5, 0.5);
+  smolKirin.position.set(WORLD_WIDTH * 0.6, WORLD_HEIGHT * 0.6);
+  smolKirin.scale.set(0.25);
+  viewport.addChild(smolKirin);
+
+  app.ticker.add(() => {
+    kirin.rotation += 0.001;
+  });
+})();
