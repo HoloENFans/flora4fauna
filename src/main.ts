6026ac6988633872c835b8d7ccbaf6638da2f213
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-12-21 21:40:31 +0700

    Rewrote comments

--- src/main.ts
+++ src/main.ts
@@ -153,7 +153,7 @@ void (async () => {
 	});
 })();
 
-// Select the modal element
+// Remove later, placeholder to test modal
 const modal: HTMLDialogElement | null = document.querySelector(
 	'dialog[is="modal-about"]',
 );
@@ -174,4 +174,3 @@ if (modal) {
 		console.log('Modal closed');
 	});
 }
-// Add footer button functionality
