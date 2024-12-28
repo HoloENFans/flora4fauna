b635a058e5148df740c6425e9d3d31061bf1f279
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2024-12-28 15:39:40 +0700

    chore(wip): filling up modals content

--- src/components/accountability-modal.ts
+++ src/components/accountability-modal.ts
@@ -21,10 +21,29 @@ export class AccountabilityModal extends LitElement {
 				.isOpen=${this.isOpen}
 				@modal-closed=${() => this.handleModalClosed()}
 			>
-				<h1 slot="header">Accountability Modal</h1>
-				<p slot="content">
-					This is the content for the Accountability Modal.
-				</p>
+				<h1 slot="header">Donation Accountability</h1>
+				<div class="separator" slot="separator"></div>
+				<div slot="content">
+					<h2>Preface</h2>
+					<p>
+						All donations are direct to the Arbor Day Foundation
+						with a payment portal from makemydonation through a hard
+						coded widget (link). This website and volunteer team
+						<strong>does not receive nor seek</strong> any of the
+						proceeds.
+					</p>
+					<br />
+					<p>
+						Our team does not collect, store, or handle the payment
+						process or financial information. Our team only handles
+						the optional messages as received from the
+						makemydonation API. Our volunteer team will not ask you
+						and will not reach out for any financial or personal
+						information. Information on impact reports for trees
+						planted from donations will be communicated with the
+						Arbor Day Foundation.
+					</p>
+				</div>
 			</base-modal>
 		`;
 	}
